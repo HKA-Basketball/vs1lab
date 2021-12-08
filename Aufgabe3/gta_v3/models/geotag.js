077b02698be194136173e42e67bb3d1fef517a46
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2021-12-08 10:06:38 +0100

    Implement GeoTag
    Co-authored-by: deco42 <deco42@users.noreply.github.com>
    Co-authored-by: Sedinq <Sedinq@users.noreply.github.com>
    Co-authored-by: elibright1 <elibright1@users.noreply.github.com>
    Co-authored-by: MrRaptorious <MrRaptorious@users.noreply.github.com>

--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -11,8 +11,93 @@
  */
 class GeoTag {
 
-    // TODO: ... your code here ...
-    
+    #latitude = 0.0;
+    #longitude = 0.0;
+    #name = '';
+    #hashtag = '';
+
+    /**
+     * 
+     * @param {number} latitude 
+     * @param {number} longitude 
+     * @param {string} name 
+     * @param {string} hashtag 
+     */
+    constructor(latitude, longitude, name, hashtag) {
+        this.#latitude = latitude;
+        this.#longitude = longitude;
+        this.#name = name;
+        this.#hashtag = hashtag;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get latitude() {
+        return this.#latitude;
+    }
+
+    /**
+     * @param {number}
+     */
+    set latitude(latitude) {
+        this.#latitude = latitude;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get longitude() {
+        return this.#longitude;
+    }
+
+    /**
+     * @param {number}
+     */
+    set longitude(longitude) {
+        this.#longitude = longitude;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+
+    /**
+     * @param {string}
+     */
+    set name(name) {
+        this.#name = name;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get hashtag() {
+        return this.#hashtag;
+    }
+
+    /**
+     * @param {string}
+     */
+    set hashtag(hashtag) {
+        this.#hashtag = hashtag;
+    }
+
+    /**
+     * Converts class variables into a Json String
+     * @returns {GeoTag}
+     */
+    toJSON() {
+        return {
+            latitude: this.#latitude,
+            longitude: this.#longitude,
+            name: this.#name,
+            hashtag: this.#hashtag
+        }
+    }
 }
 
 module.exports = GeoTag;
