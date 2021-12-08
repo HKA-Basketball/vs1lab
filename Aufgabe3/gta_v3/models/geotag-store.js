4c328e8188d08318be91f68faa25505ca1da10fd
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2021-12-08 10:07:52 +0100

    Implement GeoTagStore
    Co-authored-by: deco42 <deco42@users.noreply.github.com>
    Co-authored-by: Sedinq <Sedinq@users.noreply.github.com>
    Co-authored-by: elibright1 <elibright1@users.noreply.github.com>
    Co-authored-by: MrRaptorious <MrRaptorious@users.noreply.github.com>

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -4,6 +4,8 @@
  * This script is a template for exercise VS1lab/Aufgabe3
  * Complete all TODOs in the code documentation.
  */
+const GeoTag = require('./geotag');
+const GeoTagExamples = require('./geotag-examples');
 
 /**
  * A class for in-memory-storage of geotags
@@ -25,8 +27,73 @@
  */
 class InMemoryGeoTagStore{
 
-    // TODO: ... your code here ...
+    #geoTagList = [];
 
+    constructor() {
+        this.#geoTagList = GeoTagExamples.tagListAsGeoTags;
+    }
+
+    /**
+     * Add a geotag
+     * @param {GeoTag} geoTag 
+     */
+    addGeoTag(geoTag) {
+        this.#geoTagList.push(geoTag);
+    }
+
+    /**
+     * Delete geotags by name
+     * @param {string} name 
+     */
+    removeGeoTag(name) {
+        this.#geoTagList = this.#geoTagList.filter(geoTag => (geoTag.name !== name));
+    }
+
+    /**
+     * Calculate the distance between to locations
+     * @param {GeoTag} location1 
+     * @param {GeoTag} location2 
+     * @returns {number}
+     */
+    _distance(location1, location2) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Returns all geotags in the proximity of a location
+     * @param {GeoTag} location 
+     * @returns {GeoTag[]}
+     */
+    getNearbyGeoTags(location) {
+
+        const radius = 100; // radius in Km
+        let nearbyGeoTags = this.#geoTagList.filter(geoTag => this._distance(location, geoTag) <= radius);
+
+        return nearbyGeoTags;
+    }
+
+    /**
+     * Returns all geotags in the proximity of a location that match a keyword
+     * @param {GeoTag} location 
+     * @param {string} partName 
+     * @param {string} partHashtag 
+     * @returns {GeoTag[]}
+     */
+    searchNearbyGeoTags(location, partName, partHashtag) {
+        
+        // get all surrounding geoTags
+        let nearbyGeoTags = this.getNearbyGeoTags(location);
+
+        // if possible filter by name and/or hashtag
+        if (partName || partHashtag) {
+            nearbyGeoTags = nearbyGeoTags.filter(geoTag => { 
+                return geoTag.name.includes(partName) || geoTag.hashtag.includes(partHashtag) 
+            });
+        }
+
+        return nearbyGeoTags;
+    }
 }
 
 module.exports = InMemoryGeoTagStore
