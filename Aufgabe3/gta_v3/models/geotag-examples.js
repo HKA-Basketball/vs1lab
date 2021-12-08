bf0589ddbde9bdfb0069931ae0830689ce21386b
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2021-12-08 10:07:17 +0100

    Implement GeoTagExamples
    Co-authored-by: deco42 <deco42@users.noreply.github.com>
    Co-authored-by: Sedinq <Sedinq@users.noreply.github.com>
    Co-authored-by: elibright1 <elibright1@users.noreply.github.com>
    Co-authored-by: MrRaptorious <MrRaptorious@users.noreply.github.com>

--- Aufgabe3/gta_v3/models/geotag-examples.js
+++ Aufgabe3/gta_v3/models/geotag-examples.js
@@ -5,6 +5,8 @@
  * Complete all TODOs in the code documentation.
  */
 
+ const GeoTag = require('./geotag');
+
 /**
  * A class representing example geoTags at HKA
  * 
@@ -28,7 +30,20 @@ class GeoTagExamples {
             ['Building A', 49.015738, 8.391619, '#campus'],
             ['Building B', 49.016843, 8.391372, '#campus'],
             ['Building K', 49.013190, 8.392090, '#campus'],
+            ['Daniels hood', 49.026886, 8.392896, '#campus'],
+            ['Elias hood', 49.02107, 8.37106, '#campus'],
+            ['Noch ein punkt', 49.00100, 8.47390, '#campus'],
+            ['P1', 44.794457, 9.518599, '#campus'],
+            ['Nigeria',21.670110638322967, 11.8698258745779,'#campus'],
         ];
+    }
+
+    /**
+     * for testing purposes
+     */
+    static get tagListAsGeoTags() {
+        return this.tagList.map(tagEntry =>
+            new GeoTag(tagEntry[1], tagEntry[2], tagEntry[0], tagEntry[3]));
     }
 }
 
